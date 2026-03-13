f15f3f7463c6fdc15c5e5585c039e2ab3f65f408
sankrpri <sankritpriyanshu@gmail.com>
2026-03-13 18:20:47 +0530

    git report file

--- tests/Ey.spec.js
+++ tests/Ey.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 test("login page sucessfully login", async ({ page }) => {
   await page.goto("https://practicetestautomation.com/practice-test-login/");
   // await page.goto(`${baseURL}`);
-  await page.locator("//input[@id='username']").fill("student");
+  await page.locator(" //input[@id='username']").fill("student");
   await page.locator("//input[@id='password']").fill("Password123");
   //await page.locator("//button[text()='Submit']").click();
   await page.getByRole("button", { name: "Submit" }).click();
