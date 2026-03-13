d1e4df73c14000ad2f1acf14bd4c862068994987
sankrpri <sankritpriyanshu@gmail.com>
2026-03-13 18:02:37 +0530

    report html config file changes

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    // ["html"],
+    ["html"],
     //["allure-playwright", { outputFolder: "allure-results" }],
     ["allure-playwright", { resultsDir: "allure-results", open: "never" }],
   ],
