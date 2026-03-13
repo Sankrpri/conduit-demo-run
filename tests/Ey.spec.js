0c51bb3231fb4f13763b1d3448c37b8836544c75
sankrpri <sankritpriyanshu@gmail.com>
2026-03-13 17:37:56 +0530

    add playwright project

---
+++ tests/Ey.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from "@playwright/test";
+test("login page sucessfully login", async ({ page }) => {
+  await page.goto("https://practicetestautomation.com/practice-test-login/");
+  // await page.goto(`${baseURL}`);
+  await page.locator("//input[@id='username']").fill("student");
+  await page.locator("//input[@id='password']").fill("Password123");
+  //await page.locator("//button[text()='Submit']").click();
+  await page.getByRole("button", { name: "Submit" }).click();
+  await expect(page.locator("p.has-text-align-center")).toBeVisible();
+  await expect(page.locator("p.has-text-align-center")).toHaveText(
+    "Congratulations student. You successfully logged in!",
+  );
+});
---
+++ tests/error-message.spec.js
@@ -0,0 +1,35 @@
+import { test, expect } from "@playwright/test";
+
+[
+  {
+    username: "12",
+    errorMessage: "User is to short {minimum is 3 characters",
+    isErrorDisplayed: true,
+  },
+  { username: "123", errorMessage: "Username", isErrorDisplayed: false },
+  {
+    username: "1234567891234567890",
+    errorMessage: "Username",
+    isErrorDisplayed: false,
+  },
+  {
+    username: "12345678912345678901",
+    errorMessage: "User is to short {maximum is 20 characters",
+    isErrorDisplayed: true,
+  },
+].forEach(({ username, errorMessage, isErrorDisplayed }) => {
+  test(`Error message test ${username}`, async ({ page }) => {
+    await page.goto("https://conduit.bondaracademy.com/register");
+    await page.getByRole("textbox", { name: "username" }).fill(username);
+    await page.getByRole("textbox", { name: "Email" }).fill("12");
+    await page.getByRole("textbox", { name: "Password" }).fill("hello world");
+    await page.getByRole("button", { name: " Sign up " }).click();
+    if (isErrorDisplayed) {
+      await expect(page.locator(".error-messages")).toContainText(errorMessage);
+    } else {
+      await expect(page.locator(".error-messages")).not.toContainText(
+        errorMessage,
+      );
+    }
+  });
+});
