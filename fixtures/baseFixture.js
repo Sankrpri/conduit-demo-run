31cb1fc14f3579190e705ce1e34f82fd1ff5bce3
sankrpri <sankritpriyanshu@gmail.com>
2026-03-16 13:33:04 +0530

    yml changes

--- fixtures/baseFixture.js
+++
@@ -1,8 +0,0 @@
-import { test as base } from "@playwright/test";
-export const test = base.extend({
-  page: async ({ page }, use) => {
-    await page.goto("https://practicetestautomation.com/practice-test-login/");
-    await use(page);
-  },
-});
-export { expect } from "@playwright/test";
